b3d15e6dbf0682e60ff2f51499c2079ea6e0a8bc
Kevin Luís Alves de Lima <103067837+KevinLuisLima@users.noreply.github.com>
2025-06-18 16:46:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,18 @@ import tensorflow as tf
 import cv2
 import numpy as np
 import tempfile
-import gdown
-MODEL_ID = '1cObHXn3GtPz_WyTz3Jtkhx-Qu-BLnf8p'
-MODEL_PATH = 'Identifica_Sala_Ocupada.keras'
+import requests
 
 app = Flask(__name__)
 
-# Carrega o modelo remotamente
+MODEL_PATH = 'Identifica_Sala_Ocupada.keras'
+MODEL_URL = 'https://huggingface.co/KevinLuis/kerasModel/resolve/main/Identifica_Sala_Ocupada.keras'
+
 if not os.path.exists(MODEL_PATH):
     print("Baixando modelo...")
-    gdown.download(f"https://drive.google.com/uc?id={MODEL_ID}", MODEL_PATH, quiet=False, fuzzy=True)
-    
-    if os.path.getsize(MODEL_PATH) < 100000:
-        raise ValueError("Modelo corrompido ou link incorreto. Verifique o compartilhamento ou ID do Google Drive.")
-
+    response = requests.get(MODEL_URL)
+    with open(MODEL_PATH, 'wb') as f:
+        f.write(response.content)
 model = tf.keras.models.load_model(MODEL_PATH)
 
 def process_image_from_bytes(image_bytes):
